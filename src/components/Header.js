62ac2cd52a94efbef4fc5d36a038216c126b6490
Yoshik18 <yoshikkarnawat@gmail.com>
2020-05-09 11:53:01 +0530

    Displayed blog data into PostLayout.js, fixed navbar links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const header = props => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">{props.title}</Navbar.Brand>
+      <Navbar.Brand href="/">{props.title}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
--- src/layouts/PostLayout.js
+++ src/layouts/PostLayout.js
@@ -1,14 +1,18 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
-const BlogPost = props => {
+const BlogPost = ({ data }) => {
+  const post = data.markdownRemark
+
   return (
     <div>
       <Header></Header>
       <div className="container">
         <div className="row justify-content-md-center">
-          <h1>This is from Blog Post</h1>
+          <h1>{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
         </div>
       </div>
       <Footer></Footer>
@@ -17,3 +21,14 @@ const BlogPost = props => {
 }
 
 export default BlogPost
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
