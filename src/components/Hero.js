b9c925a83427eac7f503022135ed01376793a9ca
Yoshik18 <yoshikkarnawat@gmail.com>
2020-05-12 18:32:24 +0530

    Working on menus, error - query does not work in components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,10 @@ const Hero = () => {
   return (
     <div>
       <div className="hero mb-4 d-flex flex-column justify-content-center align-items-center text-white">
-        <h1 className="font-weight-bold underline">DigBizCards Blog</h1>
-        <p className="underline">Thoughts, stories and ideas.</p>
+        <h1 className=" display-4 font-weight-bold underline">
+          DigBizCards Blog
+        </h1>
+        <h4 className="underline">Thoughts, stories and ideas.</h4>
       </div>
     </div>
   )
